44536342ddcd9c27e8a214683658e7c29d20bd6e
Aayush <aayush@indusvision.ai>
2025-06-22 12:35:45 +0530

    Reflexion Agent

--- 3_reflexion_agent_system/chains.py
+++ 3_reflexion_agent_system/chains.py
@@ -7,7 +7,7 @@ from langchain_core.messages import HumanMessage
 
 import os
 
-from schema import AnswerQuestion
+from schema import AnswerQuestion,ReviseAnswer
 
 env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..",".env")
 load_dotenv(env_path)
@@ -35,14 +35,26 @@ actor_prompt_template = ChatPromptTemplate.from_messages(
     time= lambda:datetime.datetime.now().isoformat(),
 )
 
+revise_instructions = """
+Revise your previous using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You must include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "Reference" section to the bottom of your answer ( which does not count towards the word limit ). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information form your answer and make sure it is not more than 250 words
+"""
+
 first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction = "Provide a detailed ~250 word answer"
 )
 
-first_responder_chain = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion],tool_choice='AnswerQuestion') | parser
+first_responder_chain = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion],tool_choice='AnswerQuestion')
+
+revisor_chain = actor_prompt_template.partial(
+    first_instruction = revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer],tool_choice='ReviseAnswer') 
 
 response = first_responder_chain.invoke({
     "messages":[HumanMessage(content="Write me a blog post on how small business can leverage AI to grow")]
 })
-
-print(response)
